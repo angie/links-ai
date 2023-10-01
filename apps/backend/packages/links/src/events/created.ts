4180a7197def5b41998608dc54edda817e89ee1f
Angie Merryweather <6738119+angie@users.noreply.github.com>
2023-10-01 10:01:22 +0100

    ci: run typecheck on push

--- apps/backend/packages/links/src/events/created.ts
+++ apps/backend/packages/links/src/events/created.ts
@@ -1,6 +1,6 @@
 import { logger } from "logger";
 import { EventHandler } from "sst/node/event-bus";
-import { Events } from "../todo";
+import { Events } from "../api";
 
 export const handler = EventHandler(Events.Created, async (evt) => {
   logger.log("Todo created", evt);
