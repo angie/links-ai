fc86e0863de58c5c770aeb23c4582e7a14686d09
Angie Merryweather <6738119+angie@users.noreply.github.com>
2023-10-04 14:58:30 +0100

    style: fix eslint monorepo setup

--- apps/backend/packages/storage/src/events/categorised.ts
+++ apps/backend/packages/storage/src/events/categorised.ts
@@ -4,18 +4,27 @@ import { EventHandler } from "sst/node/event-bus";
 import { updateLink } from "../data";
 
 export const handler = EventHandler(Events.Categorised, async (evt) => {
-  const categorisedLink = evt.properties;
+  const { categories, id, summary, title, url } = evt.properties;
 
-  logger.info("Storing categorised link in database", categorisedLink);
+  logger.info("Storing categorised link in database", evt.properties);
 
   try {
-    const { data } = await updateLink(categorisedLink);
+    const { data } = await updateLink({ categories, id, summary, title, url });
 
     logger.info("Stored categorised link", { ...data });
 
-    await Events.StoredCategorised.publish(categorisedLink);
+    await Events.StoredCategorised.publish({
+      categories,
+      id,
+      summary,
+      title,
+      url,
+    });
   } catch (error) {
     logger.error(error);
-    logger.error("Failed to store link", { link: categorisedLink, error });
+    logger.error("Failed to store link", {
+      link: { categories, id, summary, title, url },
+      error,
+    });
   }
 });
