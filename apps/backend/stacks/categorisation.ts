9afd25dd0dc56081886ab640a351dc0064669743
Angie Merryweather <6738119+angie@users.noreply.github.com>
2023-10-04 19:23:14 +0100

    wip(categorisation): add chrome-aws-lambda for deployed app

--- apps/backend/stacks/categorisation.ts
+++ apps/backend/stacks/categorisation.ts
@@ -1,16 +1,35 @@
+import { LayerVersion } from "aws-cdk-lib/aws-lambda";
 import type { StackContext } from "sst/constructs";
 import { Config, use } from "sst/constructs";
 import { bus as busStack } from "./event-bus";
 import { table as tableStack } from "./table";
 
-export function linkCategorisation({ stack }: StackContext): void {
+export function linkCategorisation({ app, stack }: StackContext): void {
   const { bus } = use(busStack);
   const { table } = use(tableStack);
+
+  // https://github.com/shelfio/chrome-aws-lambda-layer
+  const layerArn =
+    "arn:aws:lambda:eu-west-2:764866452798:layer:chrome-aws-lambda:38";
+  const layer = LayerVersion.fromLayerVersionArn(
+    stack,
+    "chromeLayer",
+    layerArn,
+  );
 
   const OPENAI_API_KEY = new Config.Secret(stack, "OPENAI_API_KEY");
 
   bus.subscribe("link.stored", {
-    handler: "packages/categorisation/src/events/stored.handler",
     bind: [bus, table, OPENAI_API_KEY],
+    handler: "packages/categorisation/src/events/stored.handler",
+    runtime: "nodejs18.x",
+    timeout: 30,
+    layers: [layer],
+    nodejs: {
+      install: ["chrome-aws-lambda"],
+    },
+    environment: {
+      IS_LOCAL: app.local.toString(),
+    },
   });
 }
