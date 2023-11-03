8b812d6ff5953d18a25f02d5f4d9dbc68496bb80
Angie Merryweather <6738119+angie@users.noreply.github.com>
2023-11-03 05:54:18 +0000

    refactor(web): temp landing page for authed app

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,19 +1,13 @@
-"use client";
-
-import { useSession, signOut } from "next-auth/react";
-import { Button } from "@ui/components/button";
-import { Landing } from "../components/landing";
-
 export default function Page(): JSX.Element {
-  const { status } = useSession();
-
-  if (status === "authenticated") {
-    return (
-      <main>
-        <Button onClick={() => void signOut()}>Sign out</Button>
-      </main>
-    );
-  }
-
-  return <Landing />;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
+        <div className="mx-auto max-w-2xl text-center">
+          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
+            Main page of the main app.
+          </h2>
+        </div>
+      </div>
+    </main>
+  );
 }
