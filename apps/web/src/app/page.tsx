48d1eaf7a7be009a91dce7e07e863ca726fee103
Angie Merryweather <6738119+angie@users.noreply.github.com>
2023-11-08 08:50:32 +0000

    wip(web): add tailwindui menu bar

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,5 @@
+import Example from "../components/example";
+
 export default function Page(): JSX.Element {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
-            Main page of the main app.
-          </h2>
-        </div>
-      </div>
-    </main>
-  );
+  return <Example />;
 }
---
+++ packages/ui/components/ui/dark-light-mode-toggle.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Button } from "@ui/components/button";
+import { useTheme } from "next-themes";
+
+export function ModeToggle(): JSX.Element {
+  const { setTheme, theme } = useTheme();
+  const themeNotSelected = theme === "light" ? "dark" : "light";
+
+  return (
+    <Button
+      onClick={() => {
+        setTheme(themeNotSelected);
+      }}
+      size="icon"
+      variant="outline"
+    >
+      {theme === "light" ? (
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      ) : (
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      )}
+    </Button>
+  );
+}
